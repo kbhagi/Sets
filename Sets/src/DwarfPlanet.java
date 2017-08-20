26194ff49ceb058e992b7af0742d2fbd6d2497af
bhargavak <bhargavak37@gmail.com>
2017-08-20 14:17:26 +0530

    Sets Interfaces in Core Java , Sets as a Data structures
    Sets Interfaces in Core Java , Sets as a Data structures

---
+++ Sets/src/DwarfPlanet.java
@@ -0,0 +1,7 @@
+package bhargavak;
+
+public class DwarfPlanet extends HeavenlyBody {
+	public DwarfPlanet(String name, double orbitalPeriod) {
+		super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+	}
+}
